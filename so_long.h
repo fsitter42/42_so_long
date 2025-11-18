30a248658624f065e8984a0975822323d4b30422
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-11-18 13:49:00 +0100

    '600'

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 19:06:29 by fsitter           #+#    #+#             */
-/*   Updated: 2025/11/17 13:52:03 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/11/18 13:18:13 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,40 @@
 
 # include "libft/libft.h"
 
-
 // mapstuff.c
-int	count_lines(char *mapfile);
-char **make_map(char *mapfile);
+int			count_lines(char *mapfile);
+char		**make_map(char *mapfile);
 
 // count_stuff.c
-int count_of(char **map, char what_to_find);
+int			count_of(char **map, char what_to_find);
 
 // borders.c
-int	check_borders(char **map, int xmax, int ymax);
-int check_form(char **map);
-int check_signs(char **map);
-void find_sign(char **map, char to_find, int *x, int *y);
+int			check_borders(char **map, int xmax, int ymax);
+int			check_form(char **map);
+int			check_signs(char **map);
+void		find_sign(char **map, char to_find, int *x, int *y);
+
+// struct to safe mapdata
+struct		s_mapdata
+{
+	char	**map;
+	int		player_c;
+	int		exit_c;
+	int		coin_c;
+	int 	map_width;
+	int		map_height;
+	int		playerposition_x;
+	int		playerposition_y;
+	int		exitposition_x;
+	int		exitposition_y;
+};
+
+// struct to safe player position
+typedef struct s_point
+{
+	int		x;
+	int		y;
+}			t_point;
 
 
-
-// typedef struct s_point
-// {
-// 	int x;
-// } t_point
-
-# endif
+#endif
