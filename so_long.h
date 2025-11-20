2c20518ce8809e958a678d8428c4d48f9d834738
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-11-20 15:05:30 +0100

    struct

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 19:06:29 by fsitter           #+#    #+#             */
-/*   Updated: 2025/11/19 16:03:16 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/11/20 15:05:02 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,48 +16,67 @@
 # include "libft/libft.h"
 
 // struct to safe mapdata
-struct		s_mapdata
+typedef struct s_mapdata
 {
-	char	**map;
-	int		player_c;
-	int		exit_c;
-	int		coin_c;
-	int		map_width_x;
-	int		map_height_y;
-	int		playerposition_x;
-	int		playerposition_y;
-	int		exitposition_x;
-	int		exitposition_y;
-};
+	char				**map;
+	int					player_c;
+	int					exit_c;
+	int					coin_c;
+	int					map_width_x;
+	int					map_height_y;
+	int					playerposition_x;
+	int					playerposition_y;
+	int					exitposition_x;
+	int					exitposition_y;
+}						t_mapdata;
+
+// struct to safe image data
+typedef struct s_imagedata
+{
+	void				*img;
+	char				*xmp_path;
+	int					img_w_x;
+	int					img_h_y;
+}						t_imagedata;
+
+// struct to safe window
+typedef struct s_windata
+{
+	void				*mlx_ptr;
+	void				*win_ptr;
+	t_mapdata			*map;
+	t_imagedata			*img;
+}						t_windata;
 
 // struct to safe player position
 typedef struct s_point
 {
-	int		x;
-	int		y;
-}			t_point;
+	int					x;
+	int					y;
+}						t_point;
 
 // mapstuff.c
-int			count_lines(char *mapfile);
-char		**make_map(char *mapfile, struct s_mapdata *data);
-int			count_of(char **map, char what_to_find);
-char		**free_map(char **map);
+int						count_lines(char *mapfile);
+char					**make_map(char *mapfile, struct s_mapdata *data);
+int						count_of(char **map, char what_to_find);
+char					**free_map(char **map);
 
 // check_and_find.c
-int			check_borders(char **map, int xmax, int ymax);
-int			check_form(char **map);
-int			check_signs(char **map);
-void		find_sign(char **map, char to_find, int *x, int *y);
+int						check_borders(char **map, int xmax, int ymax);
+int						check_form(char **map);
+int						check_signs(char **map);
+void					find_sign(char **map, char to_find, int *x, int *y);
 
 // input_and_all.c
-int			check_input(int argc, char *argv);
-int			check_map_all(struct s_mapdata *data);
-int			count_all(struct s_mapdata *data);
-void		find_all(struct s_mapdata *data);
+int						check_input(int argc, char *argv);
+int						check_map_all(struct s_mapdata *data);
+int						count_all(struct s_mapdata *data);
+void					find_all(struct s_mapdata *data);
 
 // check_path.c
-int			check_path(struct s_mapdata *data, char *mapfile);
-int			fill_mapdata(struct s_mapdata *data, char *mapfile);
-int			fill_count(struct s_mapdata *data, char **cpy_map, int x, int y);
+int						check_path(struct s_mapdata *data, char *mapfile);
+int						fill_mapdata(struct s_mapdata *data, char *mapfile);
+int						fill_count(struct s_mapdata *data, char **cpy_map,
+							int x, int y);
 
 #endif
